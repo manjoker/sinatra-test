96dabfff727156b93b5cb5aa5a8178673cfc117b
manjoker <manbanhduc@gmail.com>
2014-10-14 11:01:38 +0200

    ajout d'un dossier public pour le css, et modification du fichier layout.

--- index.rb
+++ index.rb
@@ -1,9 +1,10 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'rubygems'
 
 get '/' do
 
-	erb :index
+	"bienvenue"
 end
 
 
@@ -11,6 +12,7 @@ get '/accueil' do
 
 	erb :accueil
 end
+
 
 get '/service' do
 
@@ -26,3 +28,23 @@ get '/contact' do
 
 	erb :contact
 end
+
+# __END__
+# @@ layout
+# %html
+# 	%body
+# 		= yield
+
+# @@ index
+# %h1 Bienvenue
+
+# @@service
+# %h1 service
+
+# @@contact
+# %h1 contact
+
+# @@tarif
+# %h1 tarif
+
+
